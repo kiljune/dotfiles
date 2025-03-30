9b8924b8c32e7b94c9ad8263973e8c16a87aed96
Kiljune Choi <kiljune@gmail.com>
2025-03-31 08:37:08 +0900

    add fish config files

---
+++ dot-config/fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # Aliases
+    #alias ls='ls --color'
+    #alias ll='ls -l'
+    #alias la='ls -la'
+    alias ls='lsd'
+    alias ll='ls -l'
+    alias la='ls -a'
+    alias lla='ls -la'
+    alias lt='ls --tree'
+    alias rm='rm -i'
+    alias fzf="fzf --color=dark --preview='bat {}'"
+
+    fzf --fish | source
+    starship init fish | source
+end
